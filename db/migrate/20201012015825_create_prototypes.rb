bdec08587ef07ea68385513aec51130e2b05e9da
takumi-murakami <takumi28280503@gmail.com>
2020-10-12 20:50:31 +0900

    initial commit

---
+++ db/migrate/20201012015825_create_prototypes.rb
@@ -0,0 +1,11 @@
+class CreatePrototypes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :prototypes do |t|
+      t.string :title, null:false
+      t.text :catch_copy, null: false
+      t.text :concept,  null: false
+      t.timestamps
+      t.references :user, foreign_key: true
+    end
+  end
+end
